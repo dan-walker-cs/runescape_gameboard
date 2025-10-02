284d92d2ed30946d5ac5c1276295e7a662018c7a
danla <dan.lawrence.walker@gmail.com>
2025-10-01 22:36:51 -0400

    base springboot project

---
+++ homeworld_events/hwe-app/src/main/java/com/nastyhaze/homeworld/hwe_app/HweAppApplication.java
@@ -0,0 +1,13 @@
+package com.nastyhaze.homeworld.hwe_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HweAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HweAppApplication.class, args);
+	}
+
+}
