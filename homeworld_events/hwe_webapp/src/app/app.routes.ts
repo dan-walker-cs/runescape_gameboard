395c773ef484976fe08b9e9ef00b9b25f0c488b7
danla <dan.lawrence.walker@gmail.com>
2025-10-09 20:43:54 -0400

    completed fullstack logic for rules page dynamic population

--- homeworld_events/hwe_webapp/src/app/app.routes.ts
+++ homeworld_events/hwe_webapp/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { OverviewComponent } from './features/overview/overview.component';
 import { LinksComponent } from './features/links/links.component';
 import { BoardComponent } from './features/game/components/board/board.component';
+import { RulesComponent } from './features/rules/rules.component';
 
 export const routes: Routes = [
     { path: 'overview', component: OverviewComponent },
+    { path: 'rules', component: RulesComponent },
     { path: 'board', component: BoardComponent },
     { path: 'links', component: LinksComponent },
     { path: '**', redirectTo: 'overview' } // fallback to Overview as both landing & default
--- homeworld_events/hwe_webapp/src/app/shared/constant/common-constant.ts
+++ homeworld_events/hwe_webapp/src/app/shared/constant/common-constant.ts
@@ -1,1 +1,2 @@
 export const HIGHLANDER_NUM: number = 1;
+export const ERROR_HTML: string = 'assets/templates/error.html'
