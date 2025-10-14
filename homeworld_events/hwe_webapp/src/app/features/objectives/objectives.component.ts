295c69291a061acf97c7ac9119397d36272e2699
danla <dan.lawrence.walker@gmail.com>
2025-10-14 04:03:01 -0400

    Updated objectives to be strictly static data

--- homeworld_events/hwe_webapp/src/app/features/objectives/objectives.component.ts
+++ homeworld_events/hwe_webapp/src/app/features/objectives/objectives.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { firstValueFrom } from 'rxjs';
 import { TileStore } from '../game/data/store/tile-store.service';
+import { TileModel } from '../game/models/tile.model';
 
 @Component({
     selector: 'app-objectives',
@@ -16,5 +17,14 @@ export class ObjectivesComponent {
 
     async ngOnInit(): Promise<void> {
         await firstValueFrom(this.tileStore.init());
+    }
+
+    /**
+     * Filters out non-Objective Tiles for the table display.
+     * @returns TileModel[]
+     */
+    getObjectiveTiles(): TileModel[] {
+        return this.tileStore.tiles()
+            .filter(tileModel => tileModel.weight > 0);
     }
 }
